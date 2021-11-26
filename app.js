7bf551d4f9275659038ea5af953c1f92bd3d842e
unknown <caballerosuarezsantiago@gmail.com>
2021-11-26 10:16:13 -0500

    Se agrego funcioanidad de calcular perdida por año

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.getElementById("form-M").addEventListener("submit", create);
 document.getElementById("form-M").addEventListener("submit", changeItem);
+var btnOpenPopup = document.getElementById('viewF'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btnClosePopup = document.getElementById('btn-close-popup');
+let totalForYear;
 let valueTotal;
 let valueMonth;
 
@@ -14,6 +19,7 @@ function changeItem(item) { // Dependiendo si es meses o si es año retornara el
     return cant;
 }
 
+
 function calculingPayForMonth(timePerson, cantMeta) { // Se calcula cuanto se pagara por mes.
     let m;
     valueMonth = document.getElementById("time-how").value;
@@ -24,6 +30,28 @@ function calculingPayForMonth(timePerson, cantMeta) { // Se calcula cuanto se pa
     m = new Intl.NumberFormat('es-ES').format(cantMeta / tMonth);
     cantMeta = m;
     return cantMeta;
+}
+
+function reverseFormat(item) { // Convierte un numero a entero, quita los puntos, comas y lo retornara porteriormente..
+    let c = item.replace('.', '');
+    c = c.replace('.', '');
+    c = c.replace(',', '');
+    c = parseInt(c);
+    return c;
+}
+
+function calculingLessForYear(valueTotal) { // Calcula la perdida por año.
+    let metas = JSON.parse(localStorage.getItem("Metas"));
+    let c;
+    for (var i = 0; i < metas.length; i++) {
+        if (metas[i].valueTotal === valueTotal) {
+            c = reverseFormat(valueTotal);
+            c = c * 12;
+            console.log(c);
+            c = new Intl.NumberFormat('es-ES').format((c * 4) / 100);
+        }
+    }
+    alert("Se le aconseja generar rentabilidad a su ahorro, si no lo hace su dinero perdera 4% de su dinero por la inflacion perderia la cantidad $"+c+" Por Año");
 }
 
 function create(e) {
@@ -54,6 +82,7 @@ function create(e) {
     console.log("Se guardo correctamente");
 }
 
+
 function read() {
     let metas = JSON.parse(localStorage.getItem("Metas"));
     document.getElementById("tbody").innerHTML = "";
@@ -70,9 +99,22 @@ function read() {
         <td>$ ${valueMeta}</td>
         <td><strong>$ ${valueTotal}</strong><p id="paragraph-p">La inflacion anual sera como minimo del <strong>4%</strong></p></td>
         <td><button type="button" class="btn btn-block btn-danger my-2" onclick="dele('${valueTotal}')">Eliminar</button></td>
+        <td><button type="button"  id="viewF" class="btn btn-block btn-success my-2" onclick="calculingLessForYear('${valueTotal}')">Ver consejo</button></td>
         </tr>`;
     }
 }
+
+//function view() {
+//    btnOpenPopup.addEventListener('click', function(){
+//       overlay.classList.add('active');
+//       popup.classList.add('active');
+//    });
+//    btnClosePopup.addEventListener('click', function(e){
+//        e.preventDefault();
+//        overlay.classList.remove('active');
+//        popup.classList.remove('active');
+//    });
+//}
 
 function dele(valueTotal) {
     let metas = JSON.parse(localStorage.getItem("Metas"));
@@ -86,3 +128,7 @@ function dele(valueTotal) {
     read();
 }
 read();
+//let metaAh = document.getElementsByClassName("meta-aho");
+//let valueM = document.getElementsByClassName("value-meta");
+//let valueI = document.getElementsByClassName("value-initial");
+//let timeV = document.getElementsByClassName("time-value");
